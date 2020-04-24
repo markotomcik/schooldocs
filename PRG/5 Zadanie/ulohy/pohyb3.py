48f3381275b875f2de084db9a7c3919fe6733eb7
markotomcik <markotomcik@gmail.com>
2020-04-24 14:07:29 +0200

    PRG - 5 zadanie - Úloha 3 - hotovo

--- PRG/5 Zadanie/ulohy/pohyb3.py
+++ PRG/5 Zadanie/ulohy/pohyb3.py
@@ -1,14 +1,16 @@
 import tkinter
+import math
 
 velkost=2
 
 def pohybuj_loptickou():
     for i in range(1,280):
         x=i
-        y=100/x
+        y = 10*math.sqrt(x)
+        platno.create_oval(x,y,x+velkost,y+velkost,fill="black")
         platno.coords(lopticka,x,y,x+velkost,y+velkost)
         platno.update()
-        platno.after(100)
+        platno.after(10)
         
 okno=tkinter.Tk()
 okno.title("Pohyb")    
