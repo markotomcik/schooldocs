de9fb839326667d238829a71fdd9a76d115bbfc7
markotomcik <markotomcik@gmail.com>
2020-05-17 12:13:44 +0200

    CCNA 1 - cvičenia

--- strojopis.tk/config/main.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$_SESSION[MYSQLI_HOST] = "localhost";
-$_SESSION[MYSQLI_USER] = "strojopis";
-$_SESSION[MYSQLI_PASS] = "YmL8JdHCp2S8CrZ";
-$_SESSION[MYSQLI_DB] = "strojopis";
-
-$_SESSION[DEBUG] = true;
--- strojopis.tk/signin.php
+++ strojopis.tk/signin.php
@@ -1,9 +1,11 @@
-<?php
-session_start();
 
-require_once("functions.php");
-start_page("Prihlásenie");
-
-require_once("templates/signin.template.php");
-
-end_page();
+    <form action="http://strojopis.tk/postdata.php?from=%2Fsignin.php&amp;to=%2F&amp;else=%2Fsignin.php%3Ferror%3D" method="POST">
+        <h1>Prihlásenie</h1>
+        <input type="hidden" name="id" value="" required>
+        <input type="email" name="email" id="email" placeholder="E-mailová adresa" required>
+        <input type="password" name="password" id="password" placeholder="Heslo" required>
+        <a href="signup.html">Registrovať sa</a>
+        <input type="submit" value="Prihlásiť">
+        <h1>Užitočné linky</h1>
+        Užitočné linky sa presunuli: <a href="linky.html">Užitočné linky</a>
+    </form>
